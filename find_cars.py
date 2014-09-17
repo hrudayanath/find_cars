029e7da6eaaf192ec4fa18e068f29281c7925b4b
Hruday Kommareddy <hrudayanath@gmail.com>
2014-09-16 20:05:56 -0500

    added to vantrow for searching cars

--- find_cars.py
+++ find_cars.py
@@ -6,27 +6,30 @@ import itertools
 import sys, re
 from bs4 import BeautifulSoup as bs
 
-'''
 class search_data:
-   dealer
-   url
-   terms 
+   def __init__(self, dealer,url):
+     
+      self.dealer = dealer
+      self.url = url
 
+d1 = search_data("sparks", 'http://www.sparksnissan.com/inventory/view/Used/Price/5001-10000/Transmissions/Automatic/SortBy0/')
+d2 = search_data("vantrow", 'http://vantrowtoyota.com/Monroe-Louisiana/For-Sale/Used/?MaxMSRP=10000&MinYear=2007')
+
+
+def parse_data(dealer, soup):
   if (dealer.dealer == "vantrow"):
     print "processing vantrow data"
+    for name, car_det, price in itertools.izip(soup.find_all(class_ = "vehicle-link"),soup.find_all(class_ = "vehicle-detail-list"), soup.find_all(class_ = "vehicle-price-default-price")):
+      car_name = name.string
+      car_trans = car_det.find(class_ ="trans").next_sibling.next_sibling.string
+      car_odometer =''
+      car_mpg = car_det.find(class_ ="mpg").next_sibling.next_sibling.string
+      car_price = price.get_text()
+      print car_name, car_trans, car_odometer, car_mpg, car_price 
+
   elif (dealer.dealer == "sparks"):
     print "processing sparksnissan"
-'''
-
-url = 'http://www.sparksnissan.com/inventory/view/Used/Price/5001-10000/Transmissions/Automatic/SortBy0/'
-
-titles = []
-search_terms = ['ctl00_ContentSection_ctl03_searchresults', 'vehicletitle']
-
-#search_terms = { 'vantrow': ['vehicledetails', 'pricing']}
-
-def parse_data(soup):
-   for car,price  in itertools.izip(soup.find_all(class_="left vehicleinformation"),soup.find_all(class_="single clearfix")):
+    for car,price  in itertools.izip(soup.find_all(class_="left vehicleinformation"),soup.find_all(class_="single clearfix")):
       car_name = ''.join([text for text in car.h2.get_text()])
       car_trans = car.ul.find(class_ = "vehicletrans").get_text()
       car_odometer = car.ul.find(class_ = "vehicleodometer").get_text()
@@ -37,13 +40,12 @@ def parse_data(soup):
 def get_data(url):
   return urllib2.urlopen(url)
 
-def collect_data():
-  dat = bs(get_data(url))
-  return parse_data( dat)
+def collect_data(d):
+  dat = bs(get_data(d.url))
+  return parse_data( d,dat)
     
-cars_df = collect_data()
-print cars_df
-
+collect_data(d1)
+collect_data(d2)
 '''
 new_cars = compare_fornewcars()
 send_alert(new_cars)
